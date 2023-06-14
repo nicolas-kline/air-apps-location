65216f7da34f512b2f86b01051f570909a739eb0
Nicolas Kline <93956170+nicolas-kline@users.noreply.github.com>
2023-06-14 15:48:56 -0500

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,34 @@
 import {initializeBlock} from '@airtable/blocks/ui';
 import React from 'react';
+import {Component, useState} from 'react';
+import {useCursor, useWatchable} from '@airtable/blocks/ui';
 
-function HelloWorldApp() {
-    navigator.geolocation.getCurrentPosition((loc) => {
-        console.log("Location", JSON.stringify(loc));
-    })
-    // YOUR CODE GOES HERE
-    return <div>Location Check</div>;
+const HelloWorldApp = () => {
+
+    const [location, setLocation] = useState({});
+    const cursor = useCursor();
+
+    //componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            setLocation(position);
+          console.log("Latitude is :", position.coords.latitude);
+          console.log("Longitude is :", position.coords.longitude);
+        });
+    //  }
+
+    //const [location, setLocation] = useState("");
+    //setColor("red");
+
+    // navigator.geolocation.getCurrentPosition((position) => {
+    //     setLocation(position.coords.latitude);
+    // });
+
+    return <div>Hello world 🚀 <p>{location?.coords?.latitude}</p>
+    <p>{cursor.isDataLoaded ? cursor.activeTableId : ""}</p>
+    {/* <p>{cursor.activeViewId}</p>
+    <p>{cursor.selectedRecordIds}</p>
+    <p>{cursor.selectedFieldIds}</p> */}
+    </div>
 }
 
 initializeBlock(() => <HelloWorldApp />);
